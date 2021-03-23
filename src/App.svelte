4d974599d8b0238a0065e3294e927ed8ae008e0b
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2021-03-22 21:14:58 -0600

    made the basic electron-svelte framework

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	let color = "orange";
+</script>
+
+<main>
+	<div class="background">
+		<h1>HI THERE!</h1>
+	</div>
+
+</main>
+
+<style>
+	main {
+		margin: 0;
+		padding: 0;
+	}
+
+	h1 {
+		color: #ff3eff;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	.background {
+		height: 100vh;
+		width: 100vw;
+		background: rgb(255, 128, 0);
+	}
+</style>
