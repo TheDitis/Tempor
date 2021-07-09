1c376af4f6b42a2d3b34c216d0f182bdec271d5c
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2021-07-08 22:41:38 -0600

    Converted Favorites to TypeScript

--- src/Components/Timer/Favorites/Favorites.svelte
+++ src/Components/Timer/Favorites/Favorites.svelte
@@ -1,12 +1,17 @@
-<script>
+<script lang="ts">
     import {currentFavInd, currentFavInterval, FAVORITES_KEY_MAP, intervalMode, settings} from "../../../stores/appState";
+    import {fade} from "svelte/transition";
     import _ from "lodash";
 
-    import {fade} from "svelte/transition";
+    type FavoritesItem = (number[] | number | null);
 
-    let favsList, curInd;
-
-    const handleClick = (fav, i) => clickEvent => {
+    /** Called when a favorites item is clicked
+     * @param fav {FavoritesItem} - the item in favorites corresponding to the number clicked
+     * @param i {number} - the index of the number clicked (the number minus 1)
+     * @event {KeyboardEvent} - emits event with keycode that is being listened for in MasterControls
+     *      that corresponds to the clicked favorite
+     */
+    const handleClick = (fav: FavoritesItem, i: number) => clickEvent => {
         /// Get the key that we need to fake to trigger our setter/getter function
         const keyMap = !!fav ? FAVORITES_KEY_MAP.load : FAVORITES_KEY_MAP.set;
         const key = _.invert(keyMap)[i];
@@ -14,14 +19,26 @@
         // stop event so that it doesn't remove focus from the input
         clickEvent.preventDefault();
         clickEvent.stopPropagation();
-        // Trigger the relevant function in our key listener in MasterControls
-        const e = new KeyboardEvent("keydown", {bubbles : true, cancelable : true, key, shiftKey : false});
+
+        // Trigger the relevant branch in our key listener in MasterControls
+        const e = new KeyboardEvent(
+            "keydown",
+            {bubbles : true, cancelable : true, key, shiftKey : false}
+        );
         document.dispatchEvent(e);
     };
 
-    $: { favsList = $intervalMode ? $settings.favoriteIntervals : $settings.favorites }
-    $: { curInd = $intervalMode ? $currentFavInterval : $currentFavInd; }
+    // declaring types for svelte 'reactive declaration' assignments
+    let favsList: FavoritesItem[];
+    let curInd: number;
+
+    // the list of favorites for the mode the timer is currently in
+    $: favsList = $intervalMode ? $settings.favoriteIntervals : $settings.favorites;
+    // the index of the currently selected favorite in the current mode, if a favorite is selected (for circling)
+    $: curInd = $intervalMode ? $currentFavInterval : $currentFavInd;
 </script>
+
+
 
 <div class="Favorites" in:fade={{duration: 100}} out:fade={{duration: 100}}>
     {#each favsList as fav, i}
@@ -32,30 +49,29 @@
 </div>
 
 
+
 <style>
     .Favorites {
         position: absolute;
-        top: calc((var(--size) + (var(--thickness) * 2)) * 0.24 * 1px);
+        display: flex;
         background: transparent;
         border: none;
+        top: calc((var(--size) + (var(--thickness) * 2)) * 0.24 * 1px);
         font-size: calc(var(--size) * 0.10 * 1px);
         color: var(--color);
         filter: blur(calc(var(--textBlur) * 0.8 * 1px));
-
-        display: flex;
     }
 
-
     .favorite {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
         opacity: 0.3;
         margin: 0;
         padding: 0;
         height: calc(var(--size) * 0.09 * 1px);
         width: calc(var(--size) * 0.09 * 1px);
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
         border-radius: 1000px;
         box-sizing: border-box;
         cursor: pointer;
