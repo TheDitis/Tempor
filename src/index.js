e89b63053e6f81421e23d045607b9c0c70d0bc69
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2021-07-09 23:58:47 -0600

    Started Readme!

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
+// if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+//   app.quit();
+// }
 function isDev() {
   return !app.isPackaged;
 }
